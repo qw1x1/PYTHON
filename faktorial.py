442d56b38601acbcda9772c108f6cb9087355b69
qw1x1 <qw1x.stars@gmail.com>
2022-09-25 12:24:10 +0300

    New venv LINUX

--- faktorial.py
+++
@@ -1,7 +0,0 @@
-def f(num):
-    f = 1
-    for i in range(1, num+1):
-        f *= i
-    return f
-
-print(f(int(input())))
--- get_month.py
+++
@@ -1,9 +0,0 @@
-def get_month(language, number):
-    month_ru = {1 : 'январь', 2 : 'февраль', 3 : 'март', 4 : 'апрель', 5 : 'май', 6 : 'июнь', 7 : 'июль', 8 : 'август', 9 : 'сентябрь', 10 : 'октябрь', 11 : 'ноябрь', 12 : 'декабрь'}
-    month_en = {1 : 'january', 2 : 'february', 3 : 'march', 4 : 'april', 5 : 'may', 6 : 'june', 7 : 'july', 8 : 'august', 9 : 'september', 10 : 'october', 11 : 'november', 12 : 'december'}
-    if language == 'ru':
-        return month_ru[number]
-    else:
-        return month_en[number]
-
-print(get_month(input(), int(input())))
